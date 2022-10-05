05e42c4fd4d8f79b525ca91217a79de46970fe86
Valentin Tochilkin <foxairman@gmail.com>
2022-10-05 20:54:00 +0300

    Changed handler for audio types

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 """
-Бот
+Telegram бот для конвертации голосового/аудио сообщения в текст и создания аудио из текста.
 """
 import os
 import logging
@@ -13,6 +13,7 @@ from stt_tts import STT, TTS
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+FILE_EXTENTION = ['.ogg', '.wav']
 
 bot = Bot(token=TELEGRAM_TOKEN)  # Объект бота
 dp = Dispatcher(bot)  # Диспетчер для бота
@@ -25,10 +26,10 @@ logging.basicConfig(
     filename='bot.log',
 )
 
-
-@dp.message_handler(commands="start")
+# Хэндлер на команду /start , /help
+@dp.message_handler(commands=["start", 'help'])
 async def cmd_start(message: types.Message):
-    await message.reply("Привет! Это CrazyBot! Давай веселиться!")
+    await message.reply("Привет! Это Бот для конвертации голосового/аудио сообщения в текст и создания аудио из текста.")
 
 
 # Хэндлер на команду /test
@@ -37,18 +38,10 @@ async def cmd_test(message: types.Message):
     """
     Обработчик команды /test
     """
-    # out_filename = ogg_to_wav(in_filename=r'.\voices\habr.ogg')
-    # out_filename = wav_to_ogg(in_filename=r'.\voices\test_000.wav')
-
-    # # отправка голосового сообщения
-    # path = Path("", out_filename)
-    # voice = InputFile(path)
-    # await bot.send_voice(message.from_user.id, voice)
-    # # os.remove(out_filename)
-
-    # await message.reply("Test")
+    await message.answer("Test")
 
 
+# Хэндлер на получение текста
 @dp.message_handler(content_types=[types.ContentType.TEXT])
 async def cmd_text(message: types.Message):
     """
@@ -61,60 +54,51 @@ async def cmd_text(message: types.Message):
     out_filename = tts.text_to_ogg(text)
     print(out_filename)
 
-    # отправка голосового сообщения
+    # Отправка голосового сообщения
     path = Path("", out_filename)
     voice = InputFile(path)
     await bot.send_voice(message.from_user.id, voice)
-    # os.remove(out_filename)
+    # Удаление временного файла
+    os.remove(out_filename)
 
 
-@dp.message_handler(content_types=[types.ContentType.VOICE])
+# Хэндлер на получение голосового и аудио сообщения
+@dp.message_handler(content_types=[
+    types.ContentType.VOICE,
+    types.ContentType.AUDIO,
+    types.ContentType.DOCUMENT
+    ]
+)
 async def voice_message_handler(message: types.Message):
     """
-    Обработчик на получение голосового сообщения.
+    Обработчик на получение голосового и аудио сообщения.
     """
-    file_id = message.voice.file_id
+    if message.content_type == types.ContentType.VOICE:
+        file_id = message.voice.file_id
+    elif message.content_type == types.ContentType.AUDIO:
+        file_id = message.audio.file_id
+    elif message.content_type == types.ContentType.DOCUMENT:
+        file_id = message.document.file_id
+    else:
+        await message.reply("Формат документа не поддерживается")
+        return
+
     file = await bot.get_file(file_id)
     file_path = file.file_path
-    destination = Path("tmp", f"{file_id}.ogg")
+
+    # filename, file_extension = os.path.splitext(file_path)
+    # print('filename ', filename)
+    # print('file_extension ', file_extension)
+
+    destination = Path("", f"{file_id}.tmp")
     await bot.download_file(file_path, destination=destination)
-    await message.reply("Голосовое получено и сохранено")
+    await message.reply("Аудио получено")
+
     text = stt.audio_to_text(destination)
     await message.answer(text)
+    # Удаление временного файла
+    os.remove(destination)
 
-
-@dp.message_handler(content_types=[types.ContentType.AUDIO])
-async def audio_message_handler(message: types.Message):
-    """
-    Обработчик на получение аудио.
-    """
-    file_id = message.audio.file_id
-    file = await bot.get_file(file_id)
-    file_path = file.file_path
-    print('file_path ', file_path)
-
-    destination = Path("tmp", f"{file_id}.tmp")
-    await bot.download_file(file_path, destination=destination)
-    await message.reply("Аудио получено и сохранено")
-    text = stt.audio_to_text(destination)
-    await message.answer(text)
-
-
-@dp.message_handler(content_types=[types.ContentType.DOCUMENT])
-async def document_message_handler(message: types.Message):
-    """
-    Обработчик на получение аудио.
-    """
-    file_id = message.document.file_id
-    file = await bot.get_file(file_id)
-    file_path = file.file_path
-    print('file_path ', file_path)
-
-    destination = Path("tmp", f"{file_id}.tmp")
-    await bot.download_file(file_path, destination=destination)
-    await message.reply("Документ получено и сохранено")
-    text = stt.audio_to_text(destination)
-    await message.answer(text)
 
 if __name__ == "__main__":
     # Запуск бота
@@ -123,3 +107,7 @@ if __name__ == "__main__":
         executor.start_polling(dp, skip_updates=True)
     except (KeyboardInterrupt, SystemExit):
         pass
+
+
+# поправить получение аудиофайлов и узнать их расширение
+# научиться обрабатывать в байтах
